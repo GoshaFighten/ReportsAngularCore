5461fbae93e598ba9aaf4429a0598489e6942672
goshafighten <goshafighten@gmail.com>
2017-07-27 19:43:51 +0300

    Gulp task to patch Reports scripts added

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding BeforeBuild='default' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var deleteLines = require('gulp-delete-lines');
+
+gulp.task('default', function () {
+    gulp.src(['./bower_components/xtrareportsjs/js/dx-designer.js', './bower_components/xtrareportsjs/js/web-document-viewer.js'])
+        .pipe(deleteLines({
+            'filters': [
+                /var DevExpress;/i
+            ]
+        }))
+        .pipe(gulp.dest('./bower_components/xtrareportsjs/js'));
+});
